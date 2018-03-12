81f70886cef9df85164c2c5199630e033f08a0c0
tansquire <34076657+tansquire@users.noreply.github.com>
2018-03-12 22:45:41 +0000

    Create socket.php

---
+++ socket.php
@@ -0,0 +1,35 @@
+<?php
+$con=mysqli_connect("localhost","root","123456","android");
+ 
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+ 
+// Select all of our stocks from table 'stock_tracker'
+$sql = "SELECT * FROM heroes";
+ 
+// Confirm there are results
+if ($result = mysqli_query($con, $sql))
+{
+ // We have results, create an array to hold the results
+        // and an array to hold the data
+ $resultArray = array();
+ $tempArray = array();
+ 
+ // Loop through each result
+ while($row = $result->fetch_object())
+ {
+ // Add each result into the results array
+ $tempArray = $row;
+     array_push($resultArray, $tempArray);
+ }
+ 
+ // Encode the array to JSON and output the results
+ echo json_encode($resultArray);
+}
+ 
+// Close connections
+mysqli_close($con);
+?>
